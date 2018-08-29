ea070cae72ac9d4df06565c522fcbc81daa62470
Mehdi <mehdi1712f@gmail.com>
2018-08-29 18:20:11 +0430

    first commit

---
+++ app/src/main/java/com/example/mehdi/menu/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mehdi.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause:");
+    }
+}
