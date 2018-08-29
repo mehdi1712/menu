428f4c2aaf5b8ec7f2eff45d7617f9b34a441d3d
Mehdi <mehdi1712f@gmail.com>
2018-08-29 18:25:04 +0430

    2 commit

--- app/src/main/java/com/example/mehdi/menu/MainActivity.java
+++ app/src/main/java/com/example/mehdi/menu/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        int a;
         Log.i(TAG, "onStart: ");
     }
 
